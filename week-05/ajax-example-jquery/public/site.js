5ee5118372d58e0d3e62f2d1b37eb2e4c872b881
Nick Romanidis <nick.romanidis@senecacollege.ca>
2023-03-10 13:31:15 -0500

    Week 5 code example

---
+++ week-05/ajax-example-jquery/public/site.js
@@ -0,0 +1,29 @@
+// Wait for the document to be ready ...
+$(function() {
+    $("button#getAllUsers").on("click", function() {
+        $.get("/api/users", function(objFromJson, status) {
+            console.log(objFromJson);
+            $("#lastMessage").html(objFromJson.messageHtml);
+
+            var $li = $("<li></li>").html(objFromJson.messageHtml).addClass("eventListItem");
+            $("#eventList").append($li);
+        });
+    });
+
+    $("button.getOneUser").on("click", function() {
+        var id = $(this).attr("user-id");
+
+        $.get(`/api/users/${id}`, function(objFromJson, status) {
+            console.log(objFromJson);
+            $("#lastMessage").html(objFromJson.messageHtml);
+
+            var $li = $("<li></li>").html(objFromJson.messageHtml).addClass("eventListItem");
+            $("#eventList").append($li);
+        });
+    });
+
+    $("#eventList").on("click", "li", function() {
+        $(this).css("color", "red");
+    });
+});
+
